c548f205e3dc3f04ccbfa327f2e82fc36feab463
Sergey Syrota <serg.syrota@gmail.com>
2016-07-10 10:02:34 -0500

    Adding vent control to the dashboard, and changing sprinkler controls to usable ones, instead of test.

--- getJsonData.php
+++ getJsonData.php
@@ -38,6 +38,10 @@ switch ($_GET['field']) {
         }
         echo json_encode($data);
         break;
+    case 'MasterVent':
+        $data = $rs->command('MasterVent1', 'getPosition');
+        echo json_encode(['angle' => $data]);
+        break;
     default:
         echo json_encode([]);
 }
